1006f7480af017db249495f2f97bc25dd74451f7
julianegoe <goersch.juliane@dpa.com>
2021-11-04 00:50:02 +0100

    initialize only once

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -18,19 +18,7 @@
 
 <script>
 import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
-import * as firebase from 'firebase/app';
 
-// Your web app's Firebase configuration
-const config = {
-    apiKey: 'AIzaSyB1aKO6Yial7CluAObTBBD7nMFEqGYUiV4',
-    authDomain: 'verseed.firebaseapp.com',
-    projectId: 'verseed',
-    storageBucket: 'verseed.appspot.com',
-    messagingSenderId: '149138909843',
-    appId: '1:149138909843:web:6898c1f8ccc83d8b46bb66',
-};
-
-firebase.initializeApp(config);
 const auth = getAuth();
 
     export default {
