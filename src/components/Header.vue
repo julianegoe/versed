a21b25262fff0ae54fc766189712ef873a4771be
julianegoe <goersch.juliane@dpa.com>
2021-11-04 01:14:48 +0100

    variables

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,12 +20,12 @@
 import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
 import * as firebase from 'firebase/app';
 const config = {
-    apiKey: process.env["API_KEY"],
-    authDomain: process.env["AUTH_DOMAIN"],
-    projectId: process.env["PROJECT_ID"],
-    storageBucket: process.env["STORAGE_BUCKET"],
-    messagingSenderId: process.env["MESSAGING_SENDER_ID"],
-    appId: process.env["APP_ID"],
+    apiKey: process.env["VUE_APP_API_KEY"],
+    authDomain: process.env["VUE_APP_AUTH_DOMAIN"],
+    projectId: process.env["VUE_APP_PROJECT_ID"],
+    storageBucket: process.env["VUE_APP_STORAGE_BUCKET"],
+    messagingSenderId: process.env["VUE_APP_MESSAGING_SENDER_ID"],
+    appId: process.env["VUE_APP_APP_ID"],
 };
 
 firebase.initializeApp(config);
